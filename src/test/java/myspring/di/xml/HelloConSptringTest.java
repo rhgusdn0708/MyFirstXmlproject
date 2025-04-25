ac22e9efa8ec97b96a18b8b1771bfaaff9261ae1
rhgusdn0708 <hu070821@gmail.com>
2025-04-25 13:41:06 +0900

    practice

--- src/test/java/myspring/di/xml/HelloConSptringTest.java
+++ src/test/java/myspring/di/xml/HelloConSptringTest.java
@@ -20,7 +20,18 @@ public class HelloConSptringTest {
 	void helloCons() {
 		//System.out.println(hello.sayHello());
 		assertEquals("Hello 생성자", hello.sayHello());
-		hello.print();		
+		hello.print();
+		
+		assertEquals(3, hello.getNames().size());
+		assertEquals("스프링클라우드", hello.getNames().get(2));
+		
+		//hello.getNames() ==> List<String>
+		for(String name: hello.getNames()) {
+			System.out.println(name);			
+		}
+		//Iterable인터페이스의 forEach(Consumer) 메서드 호출하기
+		//람다식 함수영 인터페이스
+		
 	}
 	
 }
